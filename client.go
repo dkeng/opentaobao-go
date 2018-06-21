00143b3568ff783d6d3cbc176c9afeb91fc1f7f2
Keng <keng@undesoft.com>
2018-06-21 10:28:25 +0800

    🎉

---
+++ client.go
@@ -0,0 +1,126 @@
+package opentaobao
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	simplejson "github.com/bitly/go-simplejson"
+)
+
+var (
+	// AppKey 应用Key
+	AppKey string
+	// AppSecret 秘密
+	AppSecret string
+	// Router 环境请求地址
+	Router string
+)
+
+// Parameter 参数
+type Parameter map[string]string
+
+//Execute 执行API接口
+func Execute(method string, param Parameter) (res *simplejson.Json, err error) {
+	err = checkConfig()
+	if err != nil {
+		return
+	}
+	param["method"] = method
+
+	req, err := http.NewRequest("POST", Router, strings.NewReader(param.getRequestData()))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+	httpClient := &http.Client{}
+	httpClient.Timeout = time.Second * 3
+
+	response, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	if response.StatusCode != 200 {
+		err = fmt.Errorf("请求错误:%d", response.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	res, err = simplejson.NewJson(body)
+	if err != nil {
+		return
+	}
+	if responseError, ok := res.CheckGet("error_response"); ok {
+		errorBytes, _ := responseError.Encode()
+		err = errors.New("执行错误:" + string(errorBytes))
+	}
+	return
+}
+
+// 检查配置
+func checkConfig() error {
+	if AppKey == "" {
+		return errors.New("AppKey 不能为空")
+	}
+	if AppSecret == "" {
+		return errors.New("AppSecret 不能为空")
+	}
+	if Router == "" {
+		return errors.New("Router 不能为空")
+	}
+	return nil
+}
+
+// 获取请求数据
+func (p *Parameter) getRequestData() string {
+	// 公共参数
+	args := url.Values{}
+	hh, _ := time.ParseDuration("8h")
+	loc := time.Now().UTC().Add(hh)
+	args.Add("timestamp", strconv.FormatInt(loc.Unix(), 10))
+	args.Add("format", "json")
+	args.Add("app_key", AppKey)
+	args.Add("v", "2.0")
+	args.Add("sign_method", "md5")
+	args.Add("partner_id", "Undesoft")
+	// 请求参数
+	for key, val := range *p {
+		args.Set(key, val)
+	}
+	// 设置签名
+	args.Add("sign", getSign(args))
+	return args.Encode()
+}
+
+// 获取签名
+func getSign(args url.Values) string {
+	// 获取Key
+	keys := []string{}
+	for k := range args {
+		keys = append(keys, k)
+	}
+	// 排序asc
+	sort.Strings(keys)
+	// 把所有参数名和参数值串在一起
+	query := AppSecret
+	for _, k := range keys {
+		query += k + args.Get(k)
+	}
+	query += AppSecret
+	// 使用MD5加密
+	signBytes := md5.Sum([]byte(query))
+	// 把二进制转化为大写的十六进制
+	return strings.ToUpper(hex.EncodeToString(signBytes[:]))
+}
--- client_test.go
+++ client_test.go
@@ -1,31 +1,25 @@
-# taobaogo
-淘宝Api、淘宝开放平台Api请求基础SDK
-
-# Example 🌰
-```go
-package main
+package opentaobao
 
 import (
 	"fmt"
-
-	 "github.com/dkeng/opentaobao-go"
+	"testing"
 )
 
 func init() {
-	opentaobao.AppKey = ""
-	opentaobao.AppSecret = ""
-	opentaobao.Router = "http://gw.api.taobao.com/router/rest"
+	AppKey = ""
+	AppSecret = ""
+	Router = "http://gw.api.taobao.com/router/rest"
 }
-
-func main() {
-	res, err := opentaobao.Execute("taobao.tbk.item.get", opentaobao.Parameter{
+func TestExecute(t *testing.T) {
+	// http://open.taobao.com/docs/api.htm?apiId=24515
+	res, err := Execute("taobao.tbk.item.get", Parameter{
 		"fields": "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick",
 		"q":      "女装",
 		"cat":    "16,18",
 	})
 
 	if err != nil {
-		fmt.Println(err)
+		t.Fatal(err)
 	}
 
 	fmt.Println("商品数量:", res.Get("tbk_item_get_response").Get("total_results").MustInt())
@@ -39,5 +33,3 @@ func main() {
 		fmt.Println("商品链接:", item["item_url"])
 	}
 }
-
-```
